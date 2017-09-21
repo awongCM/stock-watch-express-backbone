837baff331e1866694dac6688ab1a09450aebc0d
Andy Chee Ming Wong <andy@creativelicence.com.au>
2017-09-21 16:54:02 +1000

    added more stock quotes

--- public/scripts/collections/stocksDropDownCollection.js
+++ public/scripts/collections/stocksDropDownCollection.js
@@ -11,4 +11,7 @@ stockApp.stocksDropDownList = new stockApp.stocksDropDownCollection();
 
 stockApp.stocksDropDownList.add(new stockApp.stocksDropDownModel({key: "AAPL", value:"Apple"}));
 stockApp.stocksDropDownList.add(new stockApp.stocksDropDownModel({key: "FB", value:"Facebook"}));
+stockApp.stocksDropDownList.add(new stockApp.stocksDropDownModel({key: "YHOO", value:"Yahoo"}));
 stockApp.stocksDropDownList.add(new stockApp.stocksDropDownModel({key: "GOOGL", value:"Google"}));
+stockApp.stocksDropDownList.add(new stockApp.stocksDropDownModel({key: "MSFT", value:"Microsoft"}));
+stockApp.stocksDropDownList.add(new stockApp.stocksDropDownModel({key: "AMZN", value:"Amazon"}));
