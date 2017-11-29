dfd0d7c8e98b4e108a423d4c3275e79df6f6ff7a
awongCM <awong.cm@gmail.com>
2017-11-30 00:08:37 +1100

    updated todo comments

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,8 @@ var stockApp  = stockApp || {};
   stockApp.stocksDropDownList = new stockApp.stocksDropDownCollection();
   new stockApp.stocksDropDownView({collection: stockApp.stocksDropDownList});
   new stockApp.stocksFormView();
+  // TODO use collections for rendering data
   stockApp.stocksModel_instance = new stockApp.stocksModel();
+  stockApp.stocksCollection_instance = new stockApp.stocksCollection();
   new stockApp.stocksTableView({model: stockApp.stocksModel_instance});
 })();
--- scripts/collections/stocksCollections.js
+++ scripts/collections/stocksCollections.js
@@ -1,13 +1,19 @@
 // TODO: - supposed to use this collection for stocks model when making ajax call
+// var stockApp = stockApp || {};
 
-// var StocksCollection = Backbone.Collection.extend({
-//
+// stockApp.stocksCollection = Backbone.Collection.extend({
+
 // 	model: {},
 // 	url: "/api/stocks/",
-//
+
 // 	parse: function(response){
 // 		this.models = response.datasets;
-// 	}
-//
-// })
-//
+//   },
+  
+//   fetch: function() {
+//     console.log("going to fetch api");
+//     this.model = {foo:"foo", bar:"bar"};
+//   }
+
+// });
+
