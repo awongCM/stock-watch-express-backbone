2b08132cd2109cfcd311dea3f46beaee89fd3ea1
awongCM <awong.cm@gmail.com>
2019-02-24 17:40:36 +1100

    fixed and upgraded npm issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,52 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const browserSync = require('browser-sync');
-const nodemon = require('gulp-nodemon');
-const sass = require('gulp-sass');
-const del = require('del');
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const nodemon = require("gulp-nodemon");
+const sass = require("gulp-sass");
+const del = require("del");
 
-gulp.task('default', ['delete', 'scripts', 'sass', 'browser-sync'], () => {
-	gulp.watch(['./scripts/**/*.js'], ['scripts', 'browser-sync']);
-	gulp.watch(['./sass/main.sass'], ['sass', 'browser-sync']);
+gulp.task("default", ["delete", "scripts", "sass", "browser-sync"], () => {
+  gulp.watch(["./scripts/**/*.js"], ["scripts", "browser-sync"]);
+  gulp.watch(["./sass/main.sass"], ["sass", "browser-sync"]);
 });
 
-gulp.task('delete', () =>
-	del(['./public/*'])
+gulp.task("delete", () => del(["./public/*"]));
+
+gulp.task("scripts", () =>
+  gulp.src("./scripts/**/*.js").pipe(gulp.dest("./public/scripts"))
 );
 
-gulp.task('scripts', () => 
-	gulp.src('./scripts/**/*.js')
-		.pipe(gulp.dest('./public/scripts'))
+gulp.task("sass", () =>
+  gulp
+    .src("./sass/main.sass")
+    .pipe(
+      sass({
+        sourceMapEmbed: true,
+        outputStyle: "compressed"
+      }).on("error", sass.logError)
+    )
+    .pipe(gulp.dest("./public/stylesheets"))
 );
 
-gulp.task('sass', () => 
-	gulp.src('./sass/main.sass')
-		.pipe(sass({
-			sourceMapEmbed: true,
-			outputStyle: 'compressed'
-		}).on('error', sass.logError))
-		.pipe(gulp.dest('./public/stylesheets'))
-);
-
-gulp.task('browser-sync', ['nodaemon'], () => {
-    browserSync.init(null, {
-        proxy: "http://localhost:8080",
-        files: ["public/**/*.*"],
-        port: 7070
-    });
+gulp.task("browser-sync", ["nodaemon"], () => {
+  browserSync.init(null, {
+    proxy: "http://localhost:8080",
+    files: ["public/**/*.*"],
+    port: 7070
+  });
 });
 
-gulp.task('nodaemon', () => {
-    let started = false;
-	
-	return nodemon({
-		script: 'scripts/app.js'
-	}).on('start', () => {
-		if (!started) {
-			cb();
-			started = true; 
-		} 
-	});
+gulp.task("nodaemon", () => {
+  let started = false;
+
+  return nodemon({
+    script: "scripts/app.js"
+  }).on("start", () => {
+    if (!started) {
+      // don't know what happened here...
+      //   cb();
+      started = true;
+    }
+  });
 });
